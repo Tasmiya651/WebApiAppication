3ece0f636887517811bb6e9079d9c97098ff6687
Tasmiya651 <68432307+Tasmiya651@users.noreply.github.com>
2020-08-02 19:55:40 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,9 @@
+ namespace WebServer.Models{
+     public class Product{
+        public string Name{get;set;}
+          public int ID{get;set;}
+            public double Price{get;set;}
+     }
+ }
+
+ 
